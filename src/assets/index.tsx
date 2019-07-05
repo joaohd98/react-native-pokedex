40d0cb63c7167e1161e5beb533be5759ac25e81c
Joao Damazio <joaodamazio98@hotmail.com>
2019-07-05 09:11:30 -0300

    Adicionado tela de login

---
+++ src/assets/index.tsx
@@ -0,0 +1,3 @@
+export const images = {
+  pokeball: require('./images/pokeball.png'),
+};
--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import {Component} from "react";
 import {Text, View} from "react-native";
+import {Login} from "../pages/login/login";
 
 export class Layout extends Component {
 
   render(){
 
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Meu teste</Text>
-      </View>
+      <Login/>
     )
 
   }
