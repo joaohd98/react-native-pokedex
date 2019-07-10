c3db82fc1061b5d11a04442c2869a8e2290d15d7
Joao Damazio <joaodamazio98@hotmail.com>
2019-07-09 23:27:49 -0300

    Adicionado navegacao

--- App.tsx
+++ App.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
-import { Layout } from "./src/layout/layout";
+//import { Layout } from "./src/layout/layout";
+import {Pokedex} from "./src/pages/pokedex/pokedex";
+import {Detalhes} from "./src/pages/detalhes/detalhes";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+
+const AppNavigator = createStackNavigator({
+  pokedex: {
+    screen: Pokedex,
+  },
+  detalhes: {
+    screen: Detalhes,
+  },
+}, {
+  initialRouteName: 'pokedex',
+});
+
+const AppContainer = createAppContainer(AppNavigator);
 
 const App = () => {
   return (
-    <Layout/>
+    <AppContainer/>
   );
 };
 
--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,22 +1,13 @@
 import React from "react";
 import {Component} from "react";
 import {Pokedex} from "../pages/pokedex/pokedex";
-import {View} from "react-native";
-import {Header} from "../components/header/header";
 
 export class Layout extends Component {
 
   render(){
 
     return (
-      <View style={{flex: 1, flexDirection: "column"}}>
-        <View style={{flex: 0.07, backgroundColor: "#A1A"}}>
-          <Header/>
-        </View>
-        <View style={{flex: 0.93}}>
-          <Pokedex/>
-        </View>
-      </View>
+      <Pokedex/>
     )
 
   }
