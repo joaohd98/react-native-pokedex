c1bc687907a15275200fe794fb30ffd56982b775
Joao Damazio <joaodamazio98@hotmail.com>
2019-07-11 09:23:51 -0300

    Adicionado icones de font awesome

--- src/pages/pokedex/pokedex.tsx
+++ src/pages/pokedex/pokedex.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import {Component} from "react";
-import {Button, ScrollView, ViewStyle} from "react-native";
+import {Button, ScrollView} from "react-native";
 import {PokedexCard} from "./card/pokedex-card";
-import {PokedexInput} from "./input/pokedex-input";
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 interface Props {
   navigation: any
@@ -30,6 +30,7 @@ export class Pokedex extends Component<Props> {
 
     return (
       <ScrollView>
+        <Icon name="rocket" size={30} color="#900" />
         <PokedexCard />
         <PokedexCard />
         <PokedexCard />
