432773e5ee6c8b330bf106fceacdbd9574741280
Joao Damazio <joaodamazio98@hotmail.com>
2019-08-01 13:54:34 -0300

    Terminado tipos

--- src/pages/pokedex/sub-pages/filtro/tipos/filtro-tipos.tsx
+++ src/pages/pokedex/sub-pages/filtro/tipos/filtro-tipos.tsx
@@ -43,17 +43,20 @@ export class FiltroTipos extends Component<Props> {
 
     let css = FiltroCSS.TIPOS_FRAQUEZAS;
 
+    const backgroundColor = (selecionado: boolean) => selecionado ? "#30a7d7" : "#F2F2F2";
+    const selecionar = (campo: string) => this.atualizar(item, campo);
+
     return (
       <View style={css.rowTipos}>
         { this.pegarTipos(item.nome) }
         <View style={css.fraquezaForca}>
-          <TouchableWithoutFeedback onPress={() => this.atualizar(item, 'tipo')} >
-            <View style={{...css.fraquezaForcaBalao, backgroundColor: (item.tipo ? "#30a7d7" : "#F2F2F2")}}>
+          <TouchableWithoutFeedback onPress={() => selecionar('tipo')} >
+            <View style={{...css.fraquezaForcaBalao, backgroundColor: backgroundColor(item.tipo)}}>
               <Text style={css.centralizar}>T</Text>
             </View>
           </TouchableWithoutFeedback>
-          <TouchableWithoutFeedback onPress={() => this.atualizar(item, 'fraqueza')} >
-            <View style={{...css.fraquezaForcaBalao, backgroundColor: (item.fraqueza ? "#30a7d7" : "#F2F2F2")}}>
+          <TouchableWithoutFeedback onPress={() => selecionar('fraqueza')} >
+            <View style={{...css.fraquezaForcaBalao, backgroundColor: backgroundColor(item.fraqueza)}}>
               <Text style={css.centralizar}>F</Text>
             </View>
           </TouchableWithoutFeedback>
