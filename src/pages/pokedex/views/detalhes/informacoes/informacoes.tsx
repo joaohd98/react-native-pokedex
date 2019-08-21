116a58e5e17c75c23cffc62ca9b0c64f749a9ec5
Joao Damazio <joaodamazio98@hotmail.com>
2019-08-21 11:51:10 -0300

    Adicionado botao dinamico

--- src/pages/pokedex/views/detalhes/informacoes/informacoes.tsx
+++ src/pages/pokedex/views/detalhes/informacoes/informacoes.tsx
@@ -3,11 +3,21 @@ import {View, Text, TouchableOpacity} from "react-native";
 import {DetalhesCSS} from "../detalhes-css";
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-export class DetalhesInformacoes extends Component {
+interface States {
+  mostrarHabilidade: boolean,
+  alturaCard: number
+}
+
+export class DetalhesInformacoes extends Component<{}, States> {
+
+  state = {
+    mostrarHabilidade: false,
+    alturaCard: 0
+  };
 
   private css = DetalhesCSS.informacoes;
 
-  render() {
+  mostrarDescricao() {
 
     return (
       <View style={this.css.abilitiesView}>
@@ -16,7 +26,7 @@ export class DetalhesInformacoes extends Component {
             <View style={this.css.abilitiesTitle}>
               <Text style={this.css.abilitiesTitleText}>Ability Info</Text>
             </View>
-            <TouchableOpacity style={this.css.abilitiesCloseButton}>
+            <TouchableOpacity style={this.css.abilitiesCloseButton} onPress={() => this.setState({mostrarHabilidade: false})}>
               <Icon name={"close"} color={"#FFF"} size={25}/>
               <Text style={this.css.abilitiesCloseButtonText}>Close</Text>
             </TouchableOpacity>
@@ -37,49 +47,61 @@ export class DetalhesInformacoes extends Component {
 
   }
 
-}
+  mostrarCard() {
 
-{/*<View style={this.css.view}>*/}
-{/*  <View style={this.css.row}>*/}
-{/*    <View style={this.css.line}>*/}
-{/*      <Text style={this.css.word1}>Height</Text>*/}
-{/*      <Text style={this.css.word2}>1.0 m</Text>*/}
-{/*    </View>*/}
-{/*    <View style={this.css.line}>*/}
-{/*      <Text style={this.css.word1}>Category</Text>*/}
-{/*      <Text style={this.css.word2}>Seed</Text>*/}
-{/*    </View>*/}
-{/*  </View>*/}
-{/*  <View style={{...this.css.row, paddingTop: 15}}>*/}
-{/*    <View style={this.css.line}>*/}
-{/*      <Text style={this.css.word1}>Weigth</Text>*/}
-{/*      <Text style={this.css.word2}>13.0 kg</Text>*/}
-{/*    </View>*/}
-{/*    <View style={this.css.line}>*/}
-{/*      <Text style={this.css.word1}>Abilities</Text>*/}
-{/*      <View style={this.css.row}>*/}
-{/*        <View>*/}
-{/*          <Text style={this.css.word2}>*/}
-{/*            Overgrow*/}
-{/*          </Text>*/}
-{/*        </View>*/}
-{/*        <TouchableOpacity style={{marginLeft: 10}}>*/}
-{/*          <Icon name={"question-circle"} style={{fontSize: 20, color: "#FFF"}} />*/}
-{/*        </TouchableOpacity>*/}
-{/*      </View>*/}
-{/*    </View>*/}
-{/*  </View>*/}
-{/*  <View style={{...this.css.row, paddingTop: 15}}>*/}
-{/*    <View style={this.css.line}>*/}
-{/*      <Text style={this.css.word1}>Gender</Text>*/}
-{/*      <View style={this.css.row}>*/}
-{/*        <View style={{marginRight: 5}}>*/}
-{/*          <Icon name={"mars"} style={{fontSize: 25}} />*/}
-{/*        </View>*/}
-{/*        <View style={{marginLeft: 5}}>*/}
-{/*          <Icon name={"venus"} style={{fontSize: 25}}/>*/}
-{/*        </View>*/}
-{/*      </View>*/}
-{/*    </View>*/}
-{/*  </View>*/}
-{/*</View>*/}
+    return (
+      <View style={this.css.view}>
+        <View style={this.css.row}>
+          <View style={this.css.line}>
+            <Text style={this.css.word1}>Height</Text>
+            <Text style={this.css.word2}>1.0 m</Text>
+          </View>
+          <View style={this.css.line}>
+            <Text style={this.css.word1}>Category</Text>
+            <Text style={this.css.word2}>Seed</Text>
+          </View>
+        </View>
+        <View style={{...this.css.row, paddingTop: 15}}>
+          <View style={this.css.line}>
+            <Text style={this.css.word1}>Weigth</Text>
+            <Text style={this.css.word2}>13.0 kg</Text>
+          </View>
+          <View style={this.css.line}>
+            <Text style={this.css.word1}>Abilities</Text>
+            <View style={this.css.row}>
+              <View>
+                <Text style={this.css.word2}>
+                  Overgrow
+                </Text>
+              </View>
+              <TouchableOpacity style={{marginLeft: 10}} onPress={() => this.setState({mostrarHabilidade: true})}>
+                <Icon name={"question-circle"} style={{fontSize: 20, color: "#FFF"}} />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+        <View style={{...this.css.row, paddingTop: 15}}>
+          <View style={this.css.line}>
+            <Text style={this.css.word1}>Gender</Text>
+            <View style={this.css.row}>
+              <View style={{marginRight: 5}}>
+                <Icon name={"mars"} style={{fontSize: 25}} />
+              </View>
+              <View style={{marginLeft: 5}}>
+                <Icon name={"venus"} style={{fontSize: 25}}/>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+    )
+
+  }
+
+  render() {
+
+    return this.state.mostrarHabilidade ? this.mostrarDescricao() : this.mostrarCard();
+
+  }
+
+}
